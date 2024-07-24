c5164852376d442a43fe5246b85bfee4c9c3a41f
jaim1n <fazebuuug@gmail.com>
2024-07-24 16:56:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ with contextlib.suppress(ImportError):
     from pyscript import window
     input = window.prompt
 
-user_input = input("whatever you type in here will be output as hearts, minus the spaces if any (impressive, i know)")
+user_input = input("whatever you type in here will be output as hearts, minus any spaces (impressive, i know)")
 
 hearts = ""
 
