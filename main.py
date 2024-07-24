e08b3f62cc0bd51d1f381326f67ff1e46cffac13
jaim1n <fazebuuug@gmail.com>
2024-07-24 17:25:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ with contextlib.suppress(ImportError):
     from pyscript import window
     input = window.prompt
 
-user_input = input("whatever you type in here will be output as hearts, minus any spaces and special characters -- including emoji (impressive, i know):")
+user_input = input("whatever you type in here will be output as hearts, minus any spaces and special characters -- including emoji (impressive, i know) >")
 
 hearts = ""
 
